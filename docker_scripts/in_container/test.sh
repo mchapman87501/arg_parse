e773daa7727229186a64e8ad5d5ecc73f37c67f3
Mitchell Chapman <mchapman87501@gmail.com>
2022-07-13 08:50:44 -0600

    Use separate workflow steps for unit, installation tests.

--- docker_scripts/in_container/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e -u
-
-HERE=$(dirname "$0")
-echo "Here is ${HERE}."
-
-cd ${HERE}
-sh ./test_with_coverage.sh
-sh ./test_installed.sh
--- docker_scripts/on_host/run_install_test.sh
+++ docker_scripts/on_host/run_install_test.sh
@@ -3,4 +3,4 @@ set -e -u
 
 mkdir -p build_artifacts
 docker run --rm -v${PWD}:/source \
-    build_arg_parse "/source/docker_scripts/in_container/test_with_coverage.sh"
+    build_arg_parse "/source/docker_scripts/in_container/test_installed.sh"
