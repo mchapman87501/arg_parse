3e0e1b17a018c433504e67bc1c999160daad182a
Mitchell Chapman <mchapman87501@gmail.com>
2022-07-12 18:47:08 -0600

    Define a CMake package.

--- docker_scripts/in_container/test.sh
+++ docker_scripts/in_container/test.sh
@@ -11,12 +11,12 @@ cmake -DCMAKE_BUILD_TYPE=Debug /source
 # Defer checking for test failures so we can 
 # copy any test output to build_artifacts.
 set +e
-cmake --build . --target coverage_report
+cmake --build . --target arg_parse_coverage_report
 STATUS=$?
 
 set -e
 mkdir -p /source/build_artifacts
 tar cf - Testing | (cd /source/build_artifacts && tar xf - )
-tar cf - coverage_report | (cd /source/build_artifacts && tar xf - )
+tar cf - arg_parse_coverage_report | (cd /source/build_artifacts && tar xf - )
 
 exit ${STATUS}
