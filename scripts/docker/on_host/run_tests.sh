5b93254b2cd44cc87a392391187de0cdc52c189c
Mitchell Chapman <mchapman87501@gmail.com>
2022-07-17 14:21:26 -0600

    Fix typo.

--- scripts/docker/on_host/run_tests.sh
+++ scripts/docker/on_host/run_tests.sh
@@ -3,4 +3,4 @@ set -e -u
 
 mkdir -p build_artifacts
 docker run --rm -v${PWD}:/source \
-    build_arg_parse "/source/src/docker/in_container/test_with_coverage.sh"
+    build_arg_parse "/source/scripts/docker/in_container/test_with_coverage.sh"
